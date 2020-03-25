d38fb3678aa1bd7e9602d8611ade1ee487c06cf5
Ryan Parrish <rino3717@gmail.com>
2020-03-25 21:51:28 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,22 @@
 def oxford_comma(array)
-
+  
+  if array.length <=2
+    
+    array.join(", ")
+  
+    array.join(" and ")
+    
+  else
+    
+    array[0...-1].join(", ") + ", and " + array[-1]
+    
+  end
+  
 end
+
+
+  
+
+  
+
+  
