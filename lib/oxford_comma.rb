b0ccdb6d2ef43ba85c54f2179a882c2bac6ebcff
Ryan Parrish <rino3717@gmail.com>
2020-03-25 20:23:19 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,8 +2,11 @@ def oxford_comma(array)
   
   array.join(" and ")
   
-  end
+end
 
 def oxford_comma(array)
   
   array.to_sentence(words connnector: ", ", last_word_connector: " and ")
+  
+end
+  
